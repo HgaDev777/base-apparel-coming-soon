85bc4813aced8722032edce321cc50be931eced1
Markus Harrison <jodaddy15@gmail.com>
2020-02-22 06:22:22 -0600

    JS added

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,12 @@ emailBtn.addEventListener("click", checkEmail);
 function checkEmail() {
   input.placeholder = "enter valid email!";
 }
+
+//Check for pop-up blockers
+function checkForPopUp() {
+  var testPop = window.open("", "", "width=100,height=100");
+  if (testPop === null || typeof (testPop === "undefined")) {
+    alert("Please disable your popup blocker.");
+  }
+  testPop.close();
+}
